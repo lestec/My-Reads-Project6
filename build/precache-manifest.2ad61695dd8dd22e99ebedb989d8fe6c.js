1819bb852c4acf724241793b02d5428f2e37dd1f
lestec <lestec076@gmail.com>
2019-02-18 17:52:12 -0800

    added gh-pages and changes to deploy

---
+++ build/precache-manifest.2ad61695dd8dd22e99ebedb989d8fe6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "48426361ace893d53423",
+    "url": "/static/js/main.48426361.chunk.js"
+  },
+  {
+    "revision": "77f4257a6a23cc0a6f37",
+    "url": "/static/js/1.77f4257a.chunk.js"
+  },
+  {
+    "revision": "48426361ace893d53423",
+    "url": "/static/css/main.3e4255ca.chunk.css"
+  },
+  {
+    "revision": "4ab616685dd1b17759165de91f229975",
+    "url": "/index.html"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+/*import { BrowserRouter } from 'react-router-dom'*/
 
-import { BrowserRouter } from 'react-router-dom'
 ReactDOM.render(
-  <BrowserRouter>
-   <App /> 
-  </BrowserRouter>,
+  <HashRouter>
+   <App/> 
+  </HashRouter>,
   document.getElementById('root')
 )
