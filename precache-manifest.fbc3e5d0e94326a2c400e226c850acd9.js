dcb5977601b1733f3b47adb9ede8ea78cc9f44b4
lestec <lestec076@gmail.com>
2019-02-18 17:54:34 -0800

    Updates

---
+++ precache-manifest.fbc3e5d0e94326a2c400e226c850acd9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/My-Reads-Project6/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/My-Reads-Project6/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/My-Reads-Project6/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "b31e6ad75c0f0a87f952",
+    "url": "/My-Reads-Project6/static/js/runtime~main.b31e6ad7.js"
+  },
+  {
+    "revision": "beb224b516d3af116607",
+    "url": "/My-Reads-Project6/static/js/main.beb224b5.chunk.js"
+  },
+  {
+    "revision": "a34bf7d1b9873af41312",
+    "url": "/My-Reads-Project6/static/js/2.a34bf7d1.chunk.js"
+  },
+  {
+    "revision": "beb224b516d3af116607",
+    "url": "/My-Reads-Project6/static/css/main.441166c4.chunk.css"
+  },
+  {
+    "revision": "7bea4b1d2d92c50da2a9c526ce934a89",
+    "url": "/My-Reads-Project6/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/My-Reads-Project6/precache-manifest.fbc3e5d0e94326a2c400e226c850acd9.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/My-Reads-Project6/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
