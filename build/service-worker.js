107dce2a29d996f929f93099c8997b0c7e93c675
lestec <lestec076@gmail.com>
2019-02-18 19:12:10 -0800

    trying to get github pages to work

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ad61695dd8dd22e99ebedb989d8fe6c.js"
+  "/My-Reads-Project6/precache-manifest.fbc3e5d0e94326a2c400e226c850acd9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/My-Reads-Project6/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
